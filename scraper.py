0c85d315d5ec3dfecba4992a32222c59b1167d0e
elijahkzy <80436012+elijahkzy@users.noreply.github.com>
2021-06-22 00:10:27 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,6 @@ driver = webdriver.Chrome(PATH)
 driver.implicitly_wait(10)
 driver.get('https://shopee.sg/?gclid=Cj0KCQjwhr2FBhDbARIsACjwLo1PDpFJlxLU7-NGwfghB_Nw5Xhfa8IuYgyMC1jviTu_CiwSNhIOcDEaAvW5EALw_wcB')
 search = driver.find_element_by_class_name('shopee-searchbar-input__input')
-html_text = requests.get('https://shopee.sg/search?keyword=nintendo%20switch').text
 
 #search for the item in the shopee searchbar
 x = input("What are you looking for?")
@@ -34,4 +33,39 @@ for i in range(len(all_items)):
     print(all_items[i].find_element_by_xpath(".//*").text)
     print(all_links[i].get_attribute("href"))
     print(all_images[i].get_attribute("src"))
+driver.quit()
+
+#lazada
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.implicitly_wait(10)
+driver.get('https://www.lazada.sg/#')
+search = driver.find_element_by_xpath('//input[@placeholder="Search in Lazada"]')
+
+x = input("What are you looking for?")
+search.send_keys(x)
+search.send_keys(Keys.RETURN)
+
+a = driver.find_elements_by_xpath("//a[@age='0']")
+for x in a:
+    print(x.get_attribute("title"))
+    print(x.get_attribute("href"))
+driver.quit()
+
+
+#amazon
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.implicitly_wait(10)
+driver.get('https://www.amazon.sg/')
+search = driver.find_element_by_xpath('//input[@id="twotabsearchtextbox"]')
+
+x = input("What are you looking for?")
+search.send_keys(x)
+search.send_keys(Keys.RETURN)
+
+a = driver.find_elements_by_xpath("//a[@age='0']")
+for x in a:
+    print(x.get_attribute("title"))
+    print(x.get_attribute("href"))
 driver.quit()
