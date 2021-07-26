209ed33626123aef6823af8eda006c5551d64b6b
ryanklk <ryankoh0429@yahoo.com>
2021-07-26 18:27:58 +0800

    master

--- app/main.py
+++ app/main.py
@@ -18,10 +18,17 @@ import os
 app = Flask(__name__)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+chrome_options.addArguments("--window-size=1920,1080");
+chrome_options.addArguments("--disable-extensions");
+chrome_options.addArguments("--proxy-server='direct://'");
+chrome_options.addArguments("--proxy-bypass-list=*");
+chrome_options.addArguments("--start-maximized");
+chrome_options.addArguments("--headless");
+chrome_options.addArguments("--disable-gpu");
+chrome_options.addArguments("--disable-dev-shm-usage");
+chrome_options.addArguments("--no-sandbox");
+chrome_options.addArguments("--ignore-certificate-errors");
+chrome_driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 @app.route('/')
 def hello_world():
