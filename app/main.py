2166ad4919f9face199d1e06604d9a9f2b1e6707
ryanklk <ryankoh0429@yahoo.com>
2021-07-26 18:41:56 +0800

    master

--- app/main.py
+++ app/main.py
@@ -28,6 +28,9 @@ chrome_options.add_argument("--disable-gpu");
 chrome_options.add_argument("--disable-dev-shm-usage");
 chrome_options.add_argument("--no-sandbox");
 chrome_options.add_argument("--ignore-certificate-errors");
+chrome_options.add_argument("--no-first-run");
+chrome_options.add_argument("--no-default-browser-check");
+
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 @app.route('/')
